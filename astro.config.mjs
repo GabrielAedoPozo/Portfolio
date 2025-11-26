1be60afeb4cd9adb369d79326e6ab4d0a28ae6f3
Gabriel Aedo <aedothegabriel@gmail.com>
2025-11-26 17:13:21 -0500

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://portfolio-plum-omega-80.vercel.app/',
-  base: '/Portfolio/',
   vite: {
     plugins: [tailwindcss()],
   },
